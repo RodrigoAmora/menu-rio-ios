91528b56f0409abcb6c64d84b696176d10456223
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-13 11:48:11 -0300

    Commit do projeto

---
+++ MenuRio/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuRio
+//
+//  Created by Rodrigo Amora on 13/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
